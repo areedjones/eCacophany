bb3541a156a8d8e43351b98eb7f29522e3145d51
Reed Jones <a.reedjones@gmail.com>
2014-01-25 16:19:20 -0600

    Initialized repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ECacophany::Application.config.secret_key_base = 'd23eb58c35b95d703682e3362328868b2ef7cecbdcc5a8081e7b4a70780cf9e76bca845cd51635c5c614d4fa7310e52df978c5e3f1d66ee708ab14a8bf6b4768'
